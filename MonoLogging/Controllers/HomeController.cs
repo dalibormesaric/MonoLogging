8ad72eaba87f4483819741cd3b15904297df22d8
Dalibor Mesarić <dalibormesaric@gmail.com>
2014-06-11 18:32:55 +0200

    Initial commit

---
+++ MonoLogging/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Ninject.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonoLogging.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger _logger;
+
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            _logger.Info("Index page.");
+
+            return View();
+        }
+    }
+}
---
+++ MonoLogging/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MonoLogging.Global" Language="C#" %>
